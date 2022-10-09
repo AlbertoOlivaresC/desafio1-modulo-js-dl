1a1afef4b99272b287b2ce547414c17ad77cb52a
AlbertoOlivaresC <albertoolivares9597@gmail.com>
2022-10-09 17:49:05 -0400

    cambios en el css y agregué un icon

--- assets/index.js
+++ assets/index.js
@@ -7,6 +7,7 @@ const contenedorDeTotales = document.querySelector(".contenedor-oculto");
 const mostrarCostoTotal = document.querySelector("#mostrar-costo-total");
 const mostrarCantidadProductos = document.querySelector("#mostrar-cantidad-productos");
 const mostrarColorProducto = document.querySelector("#mostrar-color-producto");
+const ocultarIcon = document.querySelector("#icon");
 
 
 botonProducto.addEventListener("click", () => {
@@ -14,6 +15,7 @@ botonProducto.addEventListener("click", () => {
     //console.log(colorProducto.value);
     //totalProducto.style.color = colorProducto.value;
     //totalProducto.textContent = "El valor total es: $" + parseInt(precioProducto.textContent) * parseInt(numeroProductos.value);
+    ocultarIcon.style.display = "none";
     contenedorDeTotales.style.display = "block";
     mostrarCostoTotal.textContent = `Total: $${parseInt(precioProducto.textContent) * parseInt(numeroProductos.value)}`;
     mostrarCantidadProductos.textContent = `Cantidad: ${numeroProductos.value}`;
